115baf4d64170088976eab9823ca094dda576526
anthonyjcthomas <anthonyjcthomas@gmail.com>
2024-09-16 20:58:08 -0500

    HW1 Complete

--- badger-mart/jobs.js
+++ badger-mart/jobs.js
@@ -1,5 +1,24 @@
 function submitApplication(e) {
-    e.preventDefault(); // You can ignore this; prevents the default form submission!
+    e.preventDefault(); // Prevent the default form submission
 
-    // TODO: Alert the user of the job that they applied for!
+    // Get the list of radio inputs with the name "job"
+    const jobOptions = document.getElementsByName('job');
+    
+    // Initialize a variable to store the selected job
+    let selectedJob = '';
+
+    // Loop through the radio inputs to find the one that is checked
+    for (let i = 0; i < jobOptions.length; i++) {
+        if (jobOptions[i].checked) {
+            selectedJob = jobOptions[i].value;
+            break; // Exit the loop once the selected job is found
+        }
+    }
+
+    // Alert the user with the selected job or ask them to select a job if none is selected
+    if (selectedJob) {
+        alert(`Thank you for applying to be a ${selectedJob}!`);
+    } else {
+        alert("Please select a job!");
+    }
 }
